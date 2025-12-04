66afa5e2a40c0d5646e9c1232e19f4e84dd0888f
Brett Yance <46544627+brettyance@users.noreply.github.com>
2025-12-04 15:42:16 -0600

    Added avatar photo

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1764882164',resources: [
+    cacheName: 'chirpy-1764884496',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
